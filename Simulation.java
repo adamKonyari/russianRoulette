3ba25e00e650ffe851c144091086071c30cf61a7
adamKonyari <adam.konyari@gmail.com>
2019-01-14 11:47:39 +0100

    getter, setters in simulation added

--- Simulation.java
+++ Simulation.java
@@ -2,8 +2,30 @@ import java.util.random;
 
 public class Simulation {
 
-    public int Simulation() {
+    private int randomNumber;
+    private int weaponType = 6;
+    private int playerCount = 6;
+
+    public Simulation(int weaponType, int playerCount) {
+        this.weaponType = weaponType;
+        this.playerCount = playerCount;
         Random generator = new Random();
-        return generator.nextInt(6) + 1;
+        this.randomNumber = generator.nextInt(6) + 1;
     }
+    public int getWeaponType() {
+        return weaponType;
+    }
+
+    public void setWeaponType(int weaponType) {
+        this.weaponType = weaponType;
+    }
+
+    public int getPlayerCount() {
+        return playerCount;
+    }
+
+    public void setPlayerCount(int playerCount) {
+        this.playerCount = playerCount;
+    }
+
 }
